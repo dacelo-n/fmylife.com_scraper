dee77faa01b8ee39130238df3d5e2b0de14583a2
u03n0 <u03n0@proton.me>
2024-03-13 15:07:03 +0100

    added more features to scrape

--- scraper/fml_scraper/spiders/posts.py
+++ scraper/fml_scraper/spiders/posts.py
@@ -5,28 +5,29 @@ import scrapy
 
 class PostSpider(scrapy.Spider):
     name = "posts"
-    start_urls = [
-        "https://www.fmylife.com/",   
-    ]
+    start_urls = ["https://www.fmylife.com/"]
     base_url = "https://www.fmylife.com/?page="
     page_number = 1
 
 
-
     def parse(self, response):
-        # Extracting FML posts
         posts = response.css("article.bg-white")
         for post in posts:
-            title = post.xpath('.//h2/a/text()').get()
-            body = post.xpath('.//a[contains(@class, "text-blue-500")]/text()').get()
-            agree, deserve = post.xpath('.//div[contains(@class, "flex-wrap")]//span[contains(@class, "w-20")]/text()').getall()
+            title = post.xpath(".//h2/a/text()").get()
+            meta = post.xpath(".//p/text()").getall()
+            gender = post.xpath(".//i/@class").extract()
+            body = post.xpath(".//a[contains(@class, 'block text-blue-500 dark:text-white my-4')]/text()").get()
+            agree = post.xpath(".//div[contains(@class, 'flex vote-type-0')]//span[contains(@class, 'vote-btn-count')]/text()").get()
+            deserved = post.xpath(".//div[contains(@class, 'flex vote-type-1')]//span[contains(@class, 'vote-btn-count')]/text()").get()
 
 
             yield {
                 'title': title,
+                'meta': meta,
+                'gender': gender,
                 'body': body,
                 'agree': agree,
-                'deserve': deserve
+                'deserved': deserved
             }
         self.page_number += 1
         next_page_url = self.base_url + str(self.page_number)
